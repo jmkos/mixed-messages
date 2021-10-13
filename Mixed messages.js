e28aaae0b296a4b38fa3accbd11c4cd5e84e0b6d
Jussi Koskela <jussi_koskela@windowslive.com>
2021-10-13 17:44:00 +0300

    first commit

---
+++ Mixed messages.js
@@ -0,0 +1,5 @@
+
+const greetings = ['HELLO', 'HOWDY', 'WASSUP', 'AHOY', 'HI'];
+const compliment = ['HANDSOME', 'MR. BIG MUSCLES', 'BADASS', 'MR. WORLDS BIGGEST THIGHS', 'MR. WORLDS STRONGEST MAN'];
+const movements = ['Squats', 'Push-ups', 'Burpees', 'Lunges', 'Jumping jacks', 'Mountain climbers', 'Pull ups'];
+const repetitions = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75];
